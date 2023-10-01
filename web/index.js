fb083401183c54f523b5ffdde2995488d28e04ff
Topor Denis <contact@topor.dev>
2023-10-01 15:10:42 +0300

    Dynamic interpolation based on average delay time

--- web/index.js
+++ web/index.js
@@ -3,6 +3,14 @@ let players = [];
 let mapImg = null;
 let mapDiv = null;
 let mapData = null;
+let artificialScale = 1;
+let isAnimating = false;
+let averageTimeMs = 0;
+let currentAverageCalc = 0;
+let currentDataSize = 0;
+let USE_FAKE_DATA = false;
+var dataLog = [];
+let connectionTimeRef = null;
 /*
 player object structure;
 htmlRef; html element reference
@@ -10,21 +18,63 @@ name; name of player
 data; position [x, y, z], angle: radians
 lastUpdate: time since last update
 */
+var lastReceived = new Date();
+const initConnection = async () => {
+    if (!USE_FAKE_DATA){
+        const socket = new WebSocket("ws://188.24.175.93:22006/foo");
+        
+        socket.onopen = () => {
+            console.info('Connected to websocket.');
+        }
+        socket.onclose = () => {
+            console.info("Disconnected from websocket.");
+        }
+        socket.addEventListener("message", async (event) => {
+           
+            var obj = await event.data.text();
 
-const socket = new WebSocket("ws://188.24.175.93:22006/foo");
 
-socket.onopen = () => {
-    console.info('Connected to websocket.');
+            const diffInMs = new Date().getTime() - lastReceived.getTime();
+
+            if (averageTimeMs == 0)
+                averageTimeMs = diffInMs;
+
+            connectionTimeRef.innerText = averageTimeMs + ' MS';
+
+            if (currentAverageCalc >= 10)
+            {
+                averageTimeMs = currentAverageCalc / currentDataSize;
+                console.info('averageTimeMs ' + averageTimeMs);
+                currentDataSize = 0;
+                currentAverageCalc = 0;
+            }else{
+                currentDataSize++;
+                currentAverageCalc += diffInMs;
+            }
+
+            console.log('dif since last received data ' +  diffInMs)
+            lastReceived = new Date();
+            //console.log("Message from server ", obj);
+            await updateRadarData(JSON.parse(obj));
+            
+          })
+        }else{
+            var response = await fetch('fakeData.json');
+            dataLog = await response.json();
+            averageTimeMs = 100;
+            var count = 0;
+            setInterval(async () => {
+                if (count >= dataLog.length){
+                    count = 0;
+                }
+                await updateRadarData(dataLog[count]);
+                count++;
+                
+            }, 100);
+        }
+
 }
-socket.onclose = () => {
-    console.info("Disconnected from websocket.");
-}
-socket.addEventListener("message", async (event) => {
-   
-    var obj = await event.data.text();
-    //console.log("Message from server ", obj);
-    await updateRadarData(JSON.parse(obj));
-  })
+
 const setCurrentMap = (map) => currentMap = map;
 
 const fetchMapData = async () => {
@@ -50,14 +100,16 @@ const sampleRadarUpdate = {
         }
     }]
 }
+
 const updatePlayer = (index, data) => {
     if (players[index] == null) {
         createPlayer(index, data);
         return;
     }
 
-    players[index].data = data;
 
+    players[index].data = data;
+    //if(isAnimating) return;
     const div = players[index].htmlRef;
 
     const pos = getRadarPositionOfCoords(data.position);
@@ -65,18 +117,25 @@ const updatePlayer = (index, data) => {
 
     var bounding = htmlRef.getBoundingClientRect();
 
-    console.log('crostps ' + JSON.stringify(pos));
+  //  console.log('crostps ' + JSON.stringify(pos));
     //div.style.left = pos.x  + '%';
     //div.style.top = pos.y + '%';
     var positionInfo = mapImg.getBoundingClientRect();
     var translateX = positionInfo.width * pos.x;
     var translateY = positionInfo.height * pos.y;
-    console.log('translateX ' + translateX)
+  //  console.log('translateX ' + translateX)
 
     translateX -= bounding.width * 0.5;
     translateY -= bounding.height * 0.5;
-    div.style.transform = `translate(${translateX}px, ${translateY }px)`;
-    //div.style.transform = `translate(${translateY}px)`;
+
+    translateX /= artificialScale;
+    translateY /= artificialScale;
+
+    //div.style.top = translateY + 'px';
+   // div.style.left = translateX + 'px';
+
+    div.style.transition = `transform ${averageTimeMs}ms linear`;
+    div.style.transform = `translate(${translateX}px, ${translateY}px)`;
 }
 
 const onMapChange = async () => {
@@ -90,13 +149,17 @@ const onMapChange = async () => {
 
 }
 
+const changeArtificialScale = (scale) => {
+    artificialScale = scale;
+    mapDiv.style.transform = `scale(${scale})`
+}
 const getRadarPositionOfCoords = (coords) => {
     const mapX = (coords.x - mapData.pos_x) / mapData.scale / 1024.0;
     const mapY = (coords.y - mapData.pos_y) / mapData.scale * -1.0 / 1024.0;
 
     
 
-    console.log(mapData)
+   // console.log(mapData)
     return { x: mapX , y: mapY };
 }
 const createPlayer = (index, data) => {
@@ -114,6 +177,7 @@ const createPlayer = (index, data) => {
 
     mapDiv.appendChild(players[index].htmlRef);
 }
+
 const updateRadarData = async (radarData) => {
     console.log(radarData);
     if (radarData.map == null) return;
@@ -122,6 +186,7 @@ const updateRadarData = async (radarData) => {
         await onMapChange();
     }
 
+   
     console.log('mata');
     if (radarData.players == null) return;
 
@@ -141,7 +206,17 @@ const DOMContentLoaded = async (ev) => {
 
     document.body.appendChild(mapDiv);
 
-    //await updateRadarData(sampleRadarUpdate);
+
+    connectionTimeRef = document.createElement('div');
+    connectionTimeRef.classList.add('radar__connection');
+
+    mapDiv.appendChild(connectionTimeRef);
+
+    changeArtificialScale(1)
+
+
+    await initConnection();
+    
 }
 
 
