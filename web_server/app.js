22f9687866e8ce8cea4b78e759b9470eafdba9b0
clau <clau.burrito@gmail.com>
2023-10-02 02:09:22 +0300

    added web server files

---
+++ web_server/app.js
@@ -0,0 +1,31 @@
+import { WebSocketServer } from 'ws';
+import http from 'http';
+
+const port = 22006;
+const server = http.createServer();
+const web_socket_server = new WebSocketServer({
+    server: server, path: '/cs2_webradar'
+});
+
+web_socket_server.on("connection", (web_socket, request) => {
+    const client_address = request.socket.remoteAddress.replace("::ffff:", "");
+
+    console.log(`[info] ${client_address} connected`);
+
+    web_socket.on("message", (message) => {
+        web_socket_server.clients.forEach((client) => {
+            client.send(message);
+        });
+    });
+
+    web_socket.on("close", () => {
+        console.log(`[info] ${client_address} disconnected`);
+    });
+
+    web_socket.on("error", function (error) {
+        console.log(`[error] ${error}`);
+    });
+});
+
+server.listen(port);
+console.log(`[info] listening on port ${port}`);
