045bb157a3124caefdaa69909a1193d8990172f9
clau <clau.burrito@gmail.com>
2023-10-14 09:02:48 +0300

    parsing map name, map background image, waiting message, team check, colors

--- aolo/src/App.jsx
+++ aolo/src/App.jsx
@@ -1,13 +1,12 @@
 import { createEffect, createSignal, onMount } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { PlayerCard } from "./PlayerCard";
 import { Radar } from "./Radar";
 
-function App() {
+const App = () => {
 	const [players, setPlayers] = createSignal([]);
 	const [mapData, setMapData] = createSignal();
+	const [localTeam, setLocalTeam] = createSignal();
 
 	onMount(async () => {
 		const web_socket = new WebSocket("ws://188.24.175.93:22006/cs2_webradar");
@@ -27,10 +26,14 @@ function App() {
 		web_socket.onmessage = async (event) => {
 			const parsed_data = JSON.parse(await event.data.text());
 			setPlayers(parsed_data.players);
-		};
+			setLocalTeam(parsed_data.m_local_team);
 
-		var cristos = await (await fetch(`data/de_mirage/data.json`)).json();
-		setMapData(cristos);
+			const map = parsed_data.m_map;
+			if (map !== "invalid") {
+				setMapData({ ...(await (await fetch(`data/${map}/data.json`)).json()), name: map });
+				document.body.style.backgroundImage = `url(./data/${map}/background.png)`;
+			}
+		};
 	});
 
 	return (
@@ -41,7 +44,15 @@ function App() {
 				}</For>
 			</ul>
 
-			<Radar players={ players } image={ "./data/de_mirage/radar.png" } map_data={ mapData }></Radar>
+			{
+				players().length > 0 && mapData() && (
+					<Radar players={players} image={`./data/${mapData().name}/radar.png`} map_data={mapData} local_team={localTeam}></Radar>
+				) || (
+					<div id="radar" class="radar">
+						<h1>Waiting for map</h1>
+					</div>
+				)
+			}
 
 			<ul id="ct" class="players-list players-right">
 				<For each={players().filter((player) => player.data.m_team == 3)}>{(player) =>
--- aolo/src/Radar.jsx
+++ aolo/src/Radar.jsx
@@ -1,17 +1,50 @@
+const get_color = (color) => {
+    let new_color = "#f7d622";
+
+    switch (color) {
+        case 0:
+            new_color = "#84c8ed";
+            break;
+        case 1:
+            new_color = "#009a7d";
+            break;
+        case 2:
+            new_color = "#eadd40";
+            break;
+        case 3:
+            new_color = "#df7d29";
+            break;
+        case 4:
+            new_color = "#b72b92";
+            break;
+        default:
+            break;
+    }
+
+    return new_color;
+}
+
 export const Mot = (props) => {
-    const { data, map_data, radar_img } = props;
+    const { data, map_data, radar_img, local_team } = props;
 
     const samp = get_radar_position(map_data, data.m_position);
+
+    const mata = document.getElementsByClassName("player")[0];
+    const mata_bounding = mata && mata.getBoundingClientRect() || 0;
 
     const image_bounding = radar_img.getBoundingClientRect();
     var image_translation =
     {
-        x: (image_bounding.width * samp.x),
-        y: (image_bounding.height * samp.y)
+        x: (image_bounding.width * samp.x - mata_bounding.width * 0.5),
+        y: (image_bounding.height * samp.y - mata_bounding.height * 0.5)
     };
 
     return (
-        <div className="player" style={`transform: translate(${image_translation.x}px, ${image_translation.y}px);`}>
+        <div className="player" style={
+            `transform: translate(${image_translation.x}px, ${image_translation.y}px);
+             background-color: ${data.m_team == local_team && get_color(data.m_color) || `red`};
+             opacity: ${(data.m_is_dead || (samp.x <= 0 && samp.y <= 0)) && `0` || `1`}`
+        }>
             <div className="player__angle"></div>
         </div>
     )
@@ -37,16 +70,17 @@ const get_radar_position = (map_data, coords) => {
 }
 
 export const Radar = (props) => {
-    const { players, image, map_data } = props;
+    const { players, image, map_data, local_team } = props;
 
     let radar_img = null;
 
     return (
-        <div id="radar" class="radar">
-            <img ref={ radar_img } class="radar-image" src={ image } />
+        <div id="radar" class="radar" >
+            <img ref={radar_img} class="radar-image" src={image} />
+
             <For each={players()}>{(player) =>
-                <Mot data={ player.data } map_data={ map_data } radar_img={ radar_img }></Mot>
+                <Mot data={player.data} map_data={map_data} radar_img={radar_img} local_team={local_team()}></Mot>
             }</For>
-        </div>
+        </div >
     )
 }
