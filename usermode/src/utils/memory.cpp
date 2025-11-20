973d503ab00a1960ba30db0580a1575b9e24a56e
banbuskox <janbur008@gmail.com>
2025-11-20 18:43:26 +0100

    added handle hijack back

--- usermode/src/utils/memory.cpp
+++ usermode/src/utils/memory.cpp
@@ -15,7 +15,15 @@
 
 	this->m_id = process_id.value();
 
-	this->m_handle = OpenProcess(PROCESS_VM_READ | PROCESS_QUERY_INFORMATION, false, this->m_id);
+	auto handle = this->hijack_handle();
+	if (!handle.has_value()) {
+		this->m_handle = OpenProcess(PROCESS_VM_READ | PROCESS_QUERY_INFORMATION, false, this->m_id);
+		LOG_WARNING("Failed to hijack handle, using classic method instead.")
+	}
+	else {
+		LOG_INFO("Successfully hijacked handle.")
+			this->m_handle = handle.value();
+	}
 
 	return this->m_handle != nullptr?0:3;
 }
@@ -39,6 +47,102 @@ std::optional<uint32_t> c_memory::get_process_id(const std::string_view& process
 	}
 
 	CloseHandle(snapshot);
+	return {};
+}
+
+std::optional<void*> c_memory::hijack_handle()
+{
+	auto cleanup = [](std::vector<uint8_t>& handle_info, void*& process_handle)
+		{
+			handle_info.clear();
+
+			if (process_handle != INVALID_HANDLE_VALUE)
+			{
+				CloseHandle(process_handle);
+				process_handle = nullptr;
+			}
+		};
+
+	const auto ntdll = GetModuleHandleA("ntdll.dll");
+	if (!ntdll)
+		return {};
+
+	using fn_nt_query_system_information = long(__stdcall*)(unsigned long, void*, unsigned long, unsigned long*);
+	const auto nt_query_system_information = reinterpret_cast<fn_nt_query_system_information>(GetProcAddress(ntdll, "NtQuerySystemInformation"));
+	if (!nt_query_system_information)
+		return {};
+
+	using fn_nt_duplicate_object = long(__stdcall*)(void*, void*, void*, void**, unsigned long, unsigned long, unsigned long);
+	const auto nt_duplicate_object = reinterpret_cast<fn_nt_duplicate_object>(GetProcAddress(ntdll, "NtDuplicateObject"));
+	if (!nt_duplicate_object)
+		return {};
+
+	using fn_nt_open_process = long(__stdcall*)(void**, unsigned long, OBJECT_ATTRIBUTES*, CLIENT_ID*);
+	const auto nt_open_process = reinterpret_cast<fn_nt_open_process>(GetProcAddress(ntdll, "NtOpenProcess"));
+	if (!nt_open_process)
+		return {};
+
+	using fn_rtl_adjust_privilege = long(__stdcall*)(unsigned long, unsigned char, unsigned char, unsigned char*);
+	const auto rtl_adjust_privilege = reinterpret_cast<fn_rtl_adjust_privilege>(GetProcAddress(ntdll, "RtlAdjustPrivilege"));
+	if (!rtl_adjust_privilege)
+		return {};
+
+	uint8_t old_privilege = 0;
+	rtl_adjust_privilege(0x14, 1, 0, &old_privilege);
+
+	OBJECT_ATTRIBUTES object_attributes{};
+	InitializeObjectAttributes(&object_attributes, nullptr, 0, nullptr, nullptr);
+
+	std::vector<uint8_t> handle_info(sizeof(system_handle_info_t));
+	std::pair<void*, void*> handle{ nullptr, nullptr };
+	CLIENT_ID client_id{};
+
+	unsigned long status = 0;
+	do
+	{
+		handle_info.resize(handle_info.size() * 2);
+		status = nt_query_system_information(0x10, handle_info.data(), static_cast<unsigned long>(handle_info.size()), nullptr);
+	} while (status == 0xc0000004);
+
+	if (!NT_SUCCESS(status))
+	{
+		cleanup(handle_info, handle.first);
+		return {};
+	}
+
+	const auto system_handle_info = reinterpret_cast<system_handle_info_t*>(handle_info.data());
+	for (uint32_t idx = 0; idx < system_handle_info->m_handle_count; ++idx)
+	{
+		const auto system_handle = system_handle_info->m_handles[idx];
+		if (reinterpret_cast<void*>(system_handle.m_handle) == INVALID_HANDLE_VALUE || system_handle.m_object_type_number != 0x07)
+			continue;
+
+		client_id.UniqueProcess = reinterpret_cast<void*>(system_handle.m_process_id);
+
+		if (handle.first != nullptr && handle.first == INVALID_HANDLE_VALUE)
+		{
+			CloseHandle(handle.first);
+			continue;
+		}
+
+		const auto open_process = nt_open_process(&handle.first, PROCESS_DUP_HANDLE, &object_attributes, &client_id);
+		if (!NT_SUCCESS(open_process))
+			continue;
+
+		const auto duplicate_object = nt_duplicate_object(handle.first, reinterpret_cast<void*>(system_handle.m_handle), GetCurrentProcess(), &handle.second, PROCESS_ALL_ACCESS, 0, 0);
+		if (!NT_SUCCESS(duplicate_object))
+			continue;
+
+		if (GetProcessId(handle.second) == this->m_id)
+		{
+			cleanup(handle_info, handle.first);
+			return handle.second;
+		}
+
+		CloseHandle(handle.second);
+	}
+
+	cleanup(handle_info, handle.first);
 	return {};
 }
 
